205cf49eb6b018c7c99d05c97938a9bffe284de2
Vlad Potapov <15252vlad@gmail.com>
2021-03-30 21:17:05 +0300

    done

---
+++ src/components/TraficLightItem.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="circle" :class="[color, {on}, {blink}]"></div>
+</template>
+
+<script>
+export default {
+    name: 'TraficLightItem',
+    props: {
+        color: {
+            validator: function (value) {
+                return ['red', 'yellow', 'green'].indexOf(value) !== -1
+            },
+            required: true
+        },
+        on: {
+            type: Boolean
+        },
+        blink: {
+            type: Boolean
+        }
+    }
+}
+</script>
+
+<style scoped>
+.circle {
+    margin-bottom: 15px;
+    width: 50px;
+    height: 50px;
+    border: 1px solid #333;
+    border-radius: 25px;
+
+    opacity: 0.2;
+    border-color: #455A64;
+
+    transition-property: opacity box-shadow border-color;
+    transition-duration: .2s;
+}
+
+.circle:last-child {
+    margin-bottom: 0;
+}
+
+.on {
+    opacity: 1;
+}
+
+.red {
+    background-color: red;
+}
+
+.red.on {
+    box-shadow: 0 0 15px red;
+    border-color: red;
+}
+
+.yellow {
+    background-color: yellow;
+}
+
+.yellow.on {
+    box-shadow: 0 0 15px yellow;
+    border-color: yellow;
+}
+
+.green {
+    background-color: limegreen;
+}
+
+.green.on {
+    box-shadow: 0 0 15px limegreen;
+    border-color: limegreen;
+}
+
+@keyframes blink {
+  from {opacity: 1.0;}
+  to {opacity: 0.5;}
+}
+
+.blink {
+	animation-name: blink;
+	animation-duration: 0.5s;
+	animation-iteration-count:infinite;
+	animation-timing-function:ease-in-out;
+	animation-direction: alternate;
+}
+</style>
---
+++ src/components/TraficLitghtBody.vue
@@ -0,0 +1,134 @@
+<template>
+    <div>
+        <div class="body">
+            <TraficLightItem color="red" :on="color === 'red'" :blink="color === 'red' && timer < timerLowerBound" />
+            <TraficLightItem color="yellow" :on="color === 'yellow'" :blink="color === 'yellow' && timer < timerLowerBound"  />
+            <TraficLightItem color="green" :on="color === 'green'" :blink="color === 'green' && timer < timerLowerBound" />
+        </div>
+        <h1 class="timer" :class="{pulse: timer < timerLowerBound}">{{ timer.toFixed(1) }}</h1>
+    </div>
+</template>
+
+<script>
+import TraficLightItem from './TraficLightItem.vue';
+
+const colorMap = {
+    red: 0,
+    yellow: 1,
+    green: 2
+}
+const colorTimes = {
+    red: 10,
+    yellow: 3,
+    green: 15
+}
+
+export default {
+    name: 'TraficLightBody',
+    props: {
+        color: {
+            validator: function (value) {
+                return ['red', 'yellow', 'green'].indexOf(value) !== -1
+            },
+            required: true
+        }
+    },
+    data: function() {
+        return {
+            direction: 1,
+            timer: 0,
+            timerLowerBound: 3
+        }
+    },
+    components: { 
+        TraficLightItem 
+    },
+    mounted: function() {
+        if (localStorage.timer && (localStorage.color == this.color)) {
+            this.timer = Number(localStorage.timer);
+        } else {
+            this.timer = colorTimes[this.color];
+        }
+
+        if (localStorage.direction) {
+            this.direction = Number(localStorage.direction)
+        }
+    },
+    watch: {
+        color: function() {
+            this.timer = colorTimes[this.color];
+            localStorage.color = this.color;
+        },
+        timer: function() {
+            clearTimeout(this.timeout);
+            if (this.timer > 0) {
+                this.timeout = setTimeout(() => {
+                    this.timer -= .100;
+                }, 100)
+            } else {
+                this.changeLight();
+            }
+            localStorage.timer = this.timer
+        }
+    },
+    methods: {
+        changeLight: function() {
+            let currentLight = colorMap[this.color]
+
+            if (currentLight === 2) {
+                this.direction = -1;
+            }
+            if (currentLight === 0) {
+                this.direction = 1;
+            }
+
+            currentLight += this.direction;
+            this.$router.push('/' + Object.keys(colorMap)[currentLight]);
+
+            localStorage.direction = this.direction
+        }
+    },
+    beforeDestroy: function() {
+        clearTimeout(this.timeout);
+    }
+}
+</script>
+
+<style scoped>
+.body {
+    margin: auto;
+    width: 50px;
+    background-color: #333;
+    padding: 25px;
+    border-radius: 7px;
+    border: 2px solid #222;
+}
+
+.timer {
+    width: 70%;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: 3rem;
+    text-align: center;
+}
+
+@keyframes pulse {
+  from {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  to {
+    transform: scale(1);
+  }
+}
+
+.pulse {
+	animation-name: pulse;
+	animation-duration: 1s;
+	animation-iteration-count:infinite;
+	animation-timing-function:ease-in-out;
+	animation-direction: alternate;
+}
+</style>
